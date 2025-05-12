797e0fe23a48b1303d873555a90c47f2d24ed4de
Mekala-balaiah <balaiahyadav12@gmail.com>
2025-05-12 22:32:16 +0530

    Add files via upload

---
+++ task 1/app.js
@@ -0,0 +1,9 @@
+document.getElementById("dog").addEventListener("click",dogAlert)
+function dogAlert(){
+alert("Beware of the dogs");
+}
+
+document.getElementById("cat").addEventListener("click",catAlert)
+function catAlert(){
+alert("Cats will scratch you with their nails");
+}
